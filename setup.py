77afd615ff4008a06ecba6e4ad5687c57e05986a
Fernando André Fernandes <fernandoandrefernandes@Jumpsellers-MBP.lan>
2020-03-29 13:36:49 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="IncREC-Marko50",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Fernando André Fernandes",
     author_email="fernandoandre49@gmail.com",
     description="Software library on stream based recommender systems",
