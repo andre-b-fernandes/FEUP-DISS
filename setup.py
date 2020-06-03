2851b22ae2a03653124b44dbe10135d060cceb55
Fernando André Fernandes <fernandoandre49@gmail.com>
2020-06-03 19:23:55 +0100

    version=0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="increc",  # Replace with your own username
-    version="0.0.8",
+    version="0.0.9",
     author="Fernando André Fernandes",
     author_email="fernandoandre49@gmail.com",
     description="Software library on stream based recommender systems",
